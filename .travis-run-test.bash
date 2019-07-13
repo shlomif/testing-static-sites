6f6556386e1d512973d8752a698a50b6c31dcab9
Shlomi Fish <shlomif@shlomifish.org>
2019-07-13 21:07:37 +0300

    travis fix #3: extract script

---
+++ .travis-run-test.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# .travis-run-test.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e -x
+
+m()
+{
+    make DBTOEPUB="/usr/bin/ruby $(which dbtoepub)" DOCBOOK5_XSL_STYLESHEETS_PATH=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns "$@"
+}
+
+m
